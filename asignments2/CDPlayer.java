6f613437abeeb27a809b0874a817814414a709cb
Saeko Fukui <saeko0032@gmail.com>
2017-05-17 13:23:31 -0700

    update for cdplayer

--- asignments2/CDPlayer.java
+++ asignments2/CDPlayer.java
@@ -7,6 +7,7 @@ package assignments2;
 
 import java.util.ArrayList;
 import java.util.Scanner;
+import com.google.gson.Gson;
 
 /**
  *
@@ -27,6 +28,23 @@ public class CDPlayer {
         }
         return songList;
     }
+
+   public void getDataFromGson() {
+    	Gson gson = new Gson();
+    	Song song1 = new Song("song1title", "song1artist");
+    	Song song2 = new Song("song2title", "song2artist");
+    	Song song3 = new Song("song3title", "song4artist");
+    	Song song4 = new Song("song3title", "song4artist");
+    	Song[] songList1 = {song1, song2};
+    	Song[] songList2 = {song3,song4};
+    	CD cd1 = new CD("cdpara1-1", "cdpara1-2", songList1, false);
+    	CD cd2 = new CD("cdpara2-1", "cdpara2-2", songList2, true);
+    	CD[] cdList = {cd1,cd2};
+    //	System.out.println(gson.toJson(song1));
+    //	System.out.println(gson.toJson(songList1));
+    	System.out.println(gson.toJson(cdList));
+    }
+
 
     public void displayFirstScreen() {
         System.out.println("===============================");
